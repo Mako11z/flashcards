bcf5417e4399a4b98efcc3fcc51e6cb1a244d75d
Isaiah Gage <frb32@txstate.edu>
2023-09-29 20:56:19 -0500

    Create Project

---
+++ src/components/Flashcard.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Flashcard.css";
+
+const Flashcard = () => {
+
+    const [cardIndex, setCardIndex] = useState(0); // Holds the index of teh cards in the dictionary
+    const [showAnswer, setShowAnswer] = useState(false); // Show the answer or not
+
+    // Dictionary to hold questions and answers
+    const questionAnswer = [
+        {question: "What is the capital of Japan?", answer: "Tokyo"},
+        {question: "How many continents are there in the world?", answer: "7"},
+        {question: "Who wrote 'Romeo and Juliet'", answer: "William Shakespeare"},
+        {question: "What is the largest mammal on Earth?'", answer: "Blue Whale"},
+        {question: "What is the square root of 64?'", answer: "8"},
+        {question: "Who wrote 'Romeo and Juliet'", answer: "William Shakespeare"},
+        {question: "What is 10 * 10'", answer: "100"},
+        {question: "What is the capitol of Texas?'", answer: "Austin"},
+        {question: "How many states are there in the United States?", answer: "50"},
+    ];
+
+    const nextQA = () => {
+        setCardIndex(cardIndex + 1);
+        setShowAnswer(false);
+    }
+
+    const showTheAnswer = () => {
+        setShowAnswer(true);
+    }
+
+    return (
+        <div className="flashacards">
+            <h1>Time to Study!</h1>
+            <h2>Flashacards</h2>
+            <h3> Number of cards: {questionAnswer.length} </h3>
+            <div className="card">
+               {showAnswer ? (
+                <>
+                <p>{questionAnswer[cardIndex].answer}</p>
+                <button onClick={nextQA}>Next Question</button>
+                </>
+                ) : (
+                <>
+                <p>{questionAnswer[cardIndex].question}</p>
+                <button onClick={showTheAnswer}> show Answer </button>
+                </>
+               )
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Flashcard; 
